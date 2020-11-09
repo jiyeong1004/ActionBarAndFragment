27709c13b91a6c569090aaa2b3e0714583897e40
jiyeong1004 <ak1157@naver.com>
2020-11-09 20:26:19 +0900

    Init WebViewActivity

---
+++ app/src/main/java/com/example/actionbarandfragment/WebViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.actionbarandfragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
