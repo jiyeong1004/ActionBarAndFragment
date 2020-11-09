eef0192eb2df0f3c35251312698921c342eb7cd4
jiyeong1004 <ak1157@naver.com>
2020-11-09 21:12:29 +0900

    Complete WebViewActivity

--- app/src/main/java/com/example/actionbarandfragment/WebViewActivity.java
+++ app/src/main/java/com/example/actionbarandfragment/WebViewActivity.java
@@ -17,6 +17,7 @@ public class WebViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_web_view);
         editURL = findViewById(R.id.edit_url);
         Button btnMove = findViewById(R.id.btn_move);
